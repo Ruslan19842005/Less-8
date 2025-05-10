f1662889c1da214d85c70aaac50807f521f65a2f
yamal <yamal_07@mail.ru>
2025-05-10 20:40:22 +0900

    Задание 5

--- src/Main.java
+++ src/Main.java
@@ -16,26 +16,78 @@ public class Main {
         int number = 0;
         while (number < 10) {
             number++;
-            System.out.println(number + "");
+            System.out.print(number + " ");
         }
         System.out.println();
         for (; number >= 1; number--) {
-            System.out.println(number + "");
+            System.out.print(number + " ");
         }
 
 
         int population = 12000000;
         int fertilityPerThousand = 17;
         int mortalityPerThousand = 8;
-        int currentYear = 2025;
-        for (int year = currentYear; year < currentYear + 10; year++) {
+        int currentYearYear = 2025;
+        for (int year = currentYearYear; year < currentYearYear + 10; year++) {
             population += population * fertilityPerThousand / 1000 - population * mortalityPerThousand / 1000;
-            System.out.println(" Год " + year + " численность населения составит " + population);
+            System.out.println("Год " + year + " численность населения составит " + population);
         }
 
 
+        double savings = 15000;
+        int months = 0;
+        while (savings < 12000000) {
+            savings *= 1.07;
+            months++;
+            System.out.println("Месяц " + months + ", сумма: " + savings);
+        }
+
+
+        savings = 15000;
+        months = 0;
+        while (savings < 12000000) {
+            savings *= 1.07;
+            savings += 15000;
+            months++;
+            if (months % 6 == 0) {
+                System.out.println("Месяц " + months + ", сумма: " + savings);
+            }
+
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
