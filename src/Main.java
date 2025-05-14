35a372853925d1991203e19d7e010c41914c23f8
yamal <yamal_07@mail.ru>
2025-05-14 22:10:35 +0900

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,20 @@ public class Main {
             months++;
             System.out.println("Месяц " + months + ", сумма накоплений равна " + currentAmount + " рублей");
         }
+
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (int j = 10; j >= 1; j--) {
+            System.out.print(j + " ");
+        }
+
+
+
     }
 }
 
