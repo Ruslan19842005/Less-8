f39b05347ea467a6052c674588427b30838b9631
yamal <yamal_07@mail.ru>
2025-05-14 22:09:17 +0900

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        int desiredSum = 2459000;
-        int sum = 0;
-        int amount = 15000;
-        int month = 0;
-        while (sum < desiredSum) {
-            sum += amount;
-            month++;
-            System.out.println("Месяц " + month + "  сумма накоплений " + sum + " рублей ");
-        }
-
-
-        int number = 0;
-        while (number < 10) {
-            number++;
-            System.out.print(number + " ");
-        }
-        System.out.println();
-        for (; number >= 1; number--) {
-            System.out.print(number + " ");
-        }
-
-
-        int population = 12000000;
-        int fertilityPerThousand = 17;
-        int mortalityPerThousand = 8;
-        int currentYearYear = 2025;
-        for (int year = currentYearYear; year < currentYearYear + 10; year++) {
-            population += population * fertilityPerThousand / 1000 - population * mortalityPerThousand / 1000;
-            System.out.println("Год " + year + " численность населения составит " + population);
-        }
-
-
-        double savings = 15000;
+        int targetAmount = 2459000;
+        int monthlyDeposit = 15000;
+        int currentAmount = 0;
         int months = 0;
-        while (savings < 12000000) {
-            savings *= 1.07;
+        while (currentAmount < targetAmount) {
+            currentAmount += monthlyDeposit;
             months++;
-            System.out.println("Месяц " + months + ", сумма: " + savings);
-        }
-
-
-        savings = 15000;
-        months = 0;
-        while (savings < 12000000) {
-            savings *= 1.07;
-            savings += 15000;
-            months++;
-            if (months % 6 == 0) {
-                System.out.println("Месяц " + months + ", сумма: " + savings);
-            }
-
-
+            System.out.println("Месяц " + months + ", сумма накоплений равна " + currentAmount + " рублей");
         }
     }
 }
+
+
 
 
 
